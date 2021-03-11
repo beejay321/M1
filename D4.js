c9183b0157f57bbcbf0ccf6b6c92a87d98a29987
beejay321 <jibodu.busola@gmail.com>
2021-03-11 16:54:57 +0100

    Submitting homework

---
+++ D4.js
@@ -0,0 +1,153 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- The solution must be available for the tutors by the end of the day (5PM CET)
+- You can Google / use StackOverflow BUT we suggest you to use just the material provided
+*/
+
+/* EXERCISE 1
+Write a function "area" which receives 2 parameters (l1,l2) and calculates the area of the associated rectangle.
+*/
+const area = function(side1, side2) {
+    let value = side1 * side2
+    return value
+    }
+    console.log(area(11,12))
+
+/* EXERCISE 2
+Write a function "crazySum" which receives two integers. It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
+*/
+
+const crazySum = function(num1, num2) {
+    let result
+     if(num1 !== num2) {
+ 
+     console.log(num1 + num2)
+    } else {
+     console.log(num1 + num2 * 3)
+ 
+     }
+     return result
+ }
+
+     console.log(crazySum(27,27))
+
+     console.log(crazySum(35,75))
+
+    
+
+/* EXERCISE 3
+Write a function "crazyDiff" that computes the absolute difference between a given number and 19. 
+It should return triple their absolute difference if the given number is greater than 19.
+*/
+
+const crazyDiff = function(givNum, num19) {
+       
+    let result 
+     if(givNum > 19) {
+ 
+     console.log(3 * Math.abs(givNum - num19))
+    } else {
+     console.log(Math.abs(givNum - num19))
+ 
+     }
+     return result
+     }
+     console.log(crazyDiff(27, 19))
+
+
+/* EXERCISE 4
+Write a function "boundary" which accept an integer n and returns true if n is within 20 and 100 (included) or if it's equal to 400.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+const boundary = function (n) {
+    let result
+    if (n >= 20 && n <=100 || n === 400){
+   
+     console.log(true)
+   } return result 
+   }
+   console.log(boundary(55))
+
+
+
+/* EXERCISE 5
+Write a function "strivify" which accepts a string.
+It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+const strivify = function () {
+
+
+/* EXERCISE 6
+Write a function "check3and7" which accepts a positive number and check if it is a multiple of 3 or a multiple of 7.
+HINT: Module Operator
+*/
+
+/* WRITE YOUR CODE HERE */
+
+const check3and7 = function (aPositiveNum) {
+    let result
+    if (aPositiveNum % 3 === 0 || aPositiveNum % 7 === 0){
+   
+    console.log(true)
+   } 
+   return result 
+   }
+console.log(check3and7(21))
+
+/* EXERCISE 7
+Write a function "reverseString" to programmatically reverse a given string (es.: Strive => evirtS).
+*/
+
+/* WRITE YOUR CODE HERE */
+
+let myName = "busola"
+const reverseString = function (myName){
+
+    return myName.reverse
+ }
+console.log(reverseString(myName)) 
+
+
+/* EXERCISE 8
+Write a function "upperFirst" to capitalize the first letter of each word of a given string passed as a parameter.
+*/
+
+/* WRITE YOUR CODE HERE 
+
+let givenString = "this is my given string"
+const upperFirst = function (givenString){
+
+}
+
+*/
+
+/* EXERCISE 9
+Write a function "cutString" to create a new string without the first and last character of a given string.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+let newGivenString = "this is overwhelming"
+const cutString = function (newGivenString)
+{
+let result = newGivenString.slice(2)
+}
+console.log(cutString)
+
+
+/* EXERCISE 10
+Write a function "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* WHEN YOU ARE FINISHED
+Commit and push the code to your personal GitHub repository and share the link to your commit in Eduflow.
+*/
