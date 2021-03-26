ab899705fc5b53bd1c4897ced69500ca26597bac
beejay321 <jibodu.busola@gmail.com>
2021-03-26 05:59:08 +0100

    M1 re-organised

---
+++ M1-Benchmark/benchmark.js
@@ -0,0 +1,102 @@
+//JS EXERCISES
+
+//21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
+const printName = function () {
+  let x = "John";
+  let y = "Doe";
+  let result = x + " " + "<>" + " " + y;
+  return result;
+};
+console.log(printName());
+
+//22) Create an object with properties such name, surname, email
+
+const myDetails = function () {
+  const myPersonalDetails = {
+    name: "Oluwabusola",
+    surname: "Jibodu",
+    email: "busola.jibodu@gmail.com",
+    age: 28,
+  };
+  return myPersonalDetails;
+};
+console.log(myDetails());
+
+//23) Delete Email from the previously created object
+delete myDetails.email;
+console.log(myDetails());
+
+//24) Create an array with 10 strings in it
+
+let myColors = [
+  "red",
+  "blue",
+  "yellow",
+  "white",
+  "teal",
+  "purple",
+  "green",
+  "brown",
+  "black",
+  "pink",
+];
+
+//25) Print in the console every string in the previous array
+const newColors = function () {
+  console.log(myColors);
+
+  for (let i = 0; i < myColors.length; i++) {
+    console.log(myColors[i]);
+  }
+};
+console.log(newColors());
+
+//26) Create an array with 100 random numbers in it
+//let randomNum = Math.random() * 100
+
+const randomArray = [];
+for (let i = 0; i < 100; i++) {
+  randomNum = Math.floor(Math.random() * 1000) + 1;
+  randomArray.push(randomNum);
+}
+
+console.log(randomArray);
+
+//27) Write a function to get the MAX and the MIN from the previously created array
+
+const maxMin = function () {
+  console.log(Math.max(randomArray));
+  console.log(Math.min(randomArray));
+};
+maxMin();
+
+//28) Create an array of arrays, in which every array has 10 random numbers
+
+const arrayOfArrays = []
+    for(let i = 0; i<5; i++) {
+        
+const newRandomArray = [];
+for (let j = 0; j < 10; j++) {
+  newRandomArray.push(Math.floor(Math.random() * 100) + 1);
+}
+arrayOfArrays.push(newRandomArray)
+}
+console.log(arrayOfArrays)
+
+
+
+//29) Create a function that gets 2 arrays and returns the longest one
+const compareArrLength = function (arr1, arr2) {
+  if (arr1 > arr2) {
+    return arr1 
+  } else if (arr2 > arr1){
+    return arr2
+  } else {
+    console.log("same lenght") 
+  }
+
+}
+
+//30) Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values
+
+
